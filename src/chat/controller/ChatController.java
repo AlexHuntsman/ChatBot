1f524f2fafb13dad1b29026af63ce6835061aeb2
AlexHuntsman <ahun0442@740s-pl-118.local>
2015-10-21 08:30:22 -0600

    Started a project to create a Chatbot.

---
+++ src/chat/controller/ChatController.java
@@ -0,0 +1,13 @@
+package chat.controller;
+/**
+ * Application controller for the ChatBot project.
+ * @author ahun0442
+ * @version 1.0 10/21/15
+ */
+public class ChatController 
+{
+	public void start()
+	{
+		
+	}
+}
