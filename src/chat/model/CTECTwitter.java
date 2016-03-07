ed68c3215bc0612197cc87d4af7a7410306ac296
AlexHuntsman <ahun0442@740s-pl-118.local>
2016-03-07 09:19:09 -0700

    imported and created Twitter list

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,6 +1,12 @@
 package chat.model;
+import twitter4j.*;
+import java.util.ArrayList;
 
 public class CTECTwitter 
 {
+	private Twitter chatbotTwitter;
+	private ArrayList<Status> statuses;
+	private ArrayList<String> tweetTexts;
+	
 
 }
