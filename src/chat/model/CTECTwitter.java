79ae86d4ac4f20a9f972df22c6351f4a989b6240
AlexHuntsman <ahun0442@740s-pl-118.local>
2016-03-15 09:21:39 -0600

    Made tweetTexts and statusList Lists instead of arrayLists

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -2,23 +2,23 @@ package chat.model;
 
 import twitter4j.*;
 import java.util.*;
-
 import java.io.*;
 import chat.controller.ChatController;
 
 public class CTECTwitter
 {
 	private Twitter chatbotTwitter;
-	private ArrayList<Status> statusList;
-	private ArrayList<String> tweetTexts;
+	private List<Status> statusList;
+	private List<String> tweetTexts;
 	private ChatController baseController;
 
 	public CTECTwitter(ChatController baseController)
 	{
 		this.baseController = baseController;
 		chatbotTwitter = TwitterFactory.getSingleton();
-		statusList = new ArrayList<Status>();
-		tweetTexts = new ArrayList<String>();
+
+		List<Status> statusList = new ArrayList<Status>();
+		List<String> tweetTexts = new ArrayList<String>();
 
 	}
 
@@ -26,7 +26,9 @@ public class CTECTwitter
 	{
 		try
 		{
-			chatbotTwitter.updateStatus("Alex just tweeted from my java Chatbot " + "program! #APCSRocks!!");
+			chatbotTwitter
+					.updateStatus("Alex just tweeted from my java Chatbot "
+							+ "program! #APCSRocks!!");
 		} catch (TwitterException error)
 		{
 			baseController.handleErrors(error.getErrorMessage());
@@ -59,16 +61,16 @@ public class CTECTwitter
 
 	private String removePunctuation(String currentString)
 	{
-		//Grabs the current string
-		//Goes through each character of the string
-		//removes the punctuation from string
-		//returns the string
-		String punctuation =".,`?!:;\"(){}^[]<>-@";
-		
-		String scrubbedString ="";
-		for(int i = 0; i < currentString.length(); i++)
+		// Grabs the current string
+		// Goes through each character of the string
+		// removes the punctuation from string
+		// returns the string
+		String punctuation = ".,`?!:;\"(){}^[]<>-@";
+
+		String scrubbedString = "";
+		for (int i = 0; i < currentString.length(); i++)
 		{
-			if(punctuation.indexOf(currentString.charAt(i)) == -1)
+			if (punctuation.indexOf(currentString.charAt(i)) == -1)
 			{
 				scrubbedString += currentString.charAt(i);
 			}
@@ -76,24 +78,27 @@ public class CTECTwitter
 		return scrubbedString;
 	}
 
-	private List removeCommonEnglishWords(ArrayList<String> tweetTexts)
+	private List removeCommonEnglishWords(List<String> tweetTexts)
 	{
 		String[] boringWords = importWordsToArray();
-		
+
 		for (int count = 0; count < tweetTexts.size(); count++)
 		{
-			for(int removeSpot = 0; removeSpot < boringWords.length; removeSpot++)
+			for (int removeSpot = 0; removeSpot < boringWords.length; removeSpot++)
 			{
-				if (tweetTexts.get(count).equalsIgnoreCase(boringWords[removeSpot]))
+				if (tweetTexts.get(count).equalsIgnoreCase(
+						boringWords[removeSpot]))
 				{
 					tweetTexts.remove(count);
 					count--;
-					removeSpot = boringWords.length; // Exit the inner loop. makes sure we don't skip any words.
+					removeSpot = boringWords.length; // Exit the inner loop.
+														// makes sure we don't
+														// skip any words.
 				}
 			}
 		}
 		removeTwitterUsernameFromList(tweetTexts);
-		
+
 		return tweetTexts;
 	}
 
@@ -118,26 +123,35 @@ public class CTECTwitter
 				boringWordCount++;
 			}
 			wordFile.close();
-		}
-		catch (FileNotFoundException e)
+		} catch (FileNotFoundException e)
 		{
 			return new String[0];
 		}
 		return boringWords;
 	}
-	
-	private void removeTwitterUsernameFromList(ArrayList<String>tweetTexts)
+
+	private void removeTwitterUsernameFromList(List<String> tweetTexts)
 	{
-		
+		for (int wordCount = 0; wordCount < tweetTexts.size(); wordCount++)
+		{
+			if (tweetTexts.get(wordCount).length() >= 1
+					&& tweetTexts.get(wordCount).charAt(0) == '@')
+			{
+				tweetTexts.remove(wordCount);
+				wordCount--;
+			}
+		}
 	}
+
 	private void removeEmptyText()
 	{
-		for(int spot = 0; spot < tweetTexts.size(); spot++)
+		for (int spot = 0; spot < tweetTexts.size(); spot++)
 		{
-			if(tweetTexts.get(spot).equals(""))
+			if (tweetTexts.get(spot).equals(""))
 			{
 				tweetTexts.remove(spot);
-				//need to go backwards when you remove so that you don't skip any spots.
+				// need to go backwards when you remove so that you don't skip
+				// any spots.
 				spot--;
 			}
 		}
